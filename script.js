2279f50feabe8775e89d4c2c18673c8dde9a416b
Antonela Borgogno <antonelaborgogno@gmail.com>
2023-02-20 20:16:14 -0300

    clase03 terminada

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
     evento.preventDefault();
-    console.log("crear tarea");
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li"); //creo un nuevo elemento li
+    task.classList.add("card"); //agrego una clase card al nuevo elemento li (formato)
+    input.value = "";
+
+    const content = ` 
+    <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+
+    task.innerHTML = content; //agrego contenido al elemiento li creado
+
+    list.appendChild(task); //agrego un li(child de la ul) a la lista
+    console.log(content);
 };
 
 console.log(btn);
